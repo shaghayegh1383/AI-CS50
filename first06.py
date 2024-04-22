13693978736ce3eec4976fed2c4d3284ad450fb6
shaghayegh1383 <sh.s.m1383@gmail.com>
2024-04-22 11:45:06 +0330

    append & sort

--- first06.py
+++ first06.py
@@ -1,7 +1,7 @@
 c = ["david", "ali", "barbod", "akbar"]
 
 a = input("Enter a name: ")
-c.append(a)
-c.sort()
-for i in range(len(c)):
+c.append(a) #اسم گرفته شده رو به اخر لیست اضافه میکنه
+c.sort()  #بر اساس حروف الفبا مرتب میکنه
+for i in range(len(c)): #len طولش یعنی تعداد اسمایی که تو لیسته رو میده
     print(c[i])
